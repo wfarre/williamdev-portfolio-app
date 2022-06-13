a4f91e929b9cbd84c64538ee2197a367952ae6e4
william.farre@gmail.com <william.farre@gmail.com>
2022-06-13 15:31:11 +0800

    add responsiveness

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,10 @@ function Header(){
         <header className='header'>
           <span className='header__hello'>Hello, my name is</span>
           <h1 className='header__title'>William Farré.</h1>
-          <h2 className='header__title--sub'>I love making website!</h2>
+          <h2 className='header__title--sub'>I love making websites!</h2>
           <p className='header__presentation'>I am a frontend web developper with a 
           previous<br/> background in customer service.</p>
-          <button className='btn btn-contact'>Contact Me</button>
+          <a href='mailto:william.farre@gmail.com' className='btn btn-contact'>Contact Me</a>
         </header>
     )
 }
